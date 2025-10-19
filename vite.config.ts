c31bc0f458ab88e65c479ed17b8d78e0b4dd8e58
navee-basha <naveedbasha9313@gmail.com>
2025-10-19 19:50:08 +0530

    six

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@
   import react from '@vitejs/plugin-react-swc';
   import path from 'path';
   import tailwindcss from '@tailwindcss/vite';
+  // vite.config.js
 
   export default defineConfig({
     plugins: [react(), tailwindcss(),
@@ -54,12 +55,12 @@
         '@': path.resolve(__dirname, './src'),
       },
     },
-    build: {
-      target: 'esnext',
-      outDir: 'build',
-    },
-    server: {
-      port: 3000,
-      open: true,
-    },
-  });
+  build: {
+    target: 'esnext',
+    outDir: 'build',
+  },
+  server: {
+    port: 3000,
+    open: true,
+  },
+});
